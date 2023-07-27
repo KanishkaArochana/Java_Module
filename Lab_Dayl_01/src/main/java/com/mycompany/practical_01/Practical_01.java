48757d790cb2b7b20a841f835cb435f0556d66bc
KanishkaArochana <135253502+KanishkaArochana@users.noreply.github.com>
2023-07-27 23:49:44 +0530

    Add files via upload

---
+++ Lab_Dayl_01/src/main/java/com/mycompany/practical_01/Practical_01.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practical_01;
+
+/**
+ *
+ * @author kanis
+ */
+public class Practical_01 {
+
+    public static void main(String[] args) {
+     /*   System.out.println("Hello World!");
+        System.out.println("Kanishka Arochana");
+        System.out.println("Software Engineer");
+        
+        for(int i=0;i<5;i++){
+            System.out.println("Executing Loop "+i);
+        }*/
+        
+       //4
+    /*   int [] numbers={10,20,30,40,50};
+       for(int x:numbers){
+           if(x==30){
+               break;
+           }
+           System.out.print(x);
+           System.out.print("\n");
+       }
+        System.out.println("I’m  out of the Loop now");*/
+        
+        //2 Methods
+           /*   int [] numbers={10,20,30,40,50};
+       for(int x:numbers){
+           if(x==30){
+               continue;
+           }
+           System.out.print(x);
+           System.out.print("\n");
+       }
+        System.out.println("I’m  out of the Loop now");*/
+    
+    //5
+   /* char grade='A';
+    switch(grade){
+        case 'A':System.out.println("Excellent!");
+        break;
+        
+        case'D':System.out.println("You passed");
+        break;
+        
+          case'F':System.out.println("Better try again");
+        break;
+        
+          default:System.out.println("Invalid grade");
+          
+             
+        
+        }
+             System.out.println("Your grade is "+grade); */
+             
+             
+           //2 Methods
+          /* char grade='A';
+    switch(grade){
+        case 'A':System.out.println("Excellent!");
+        
+        
+        case'D':System.out.println("You passed");
+        
+        
+          case'F':System.out.println("Better try again");
+        
+        
+          default:System.out.println("Invalid grade");
+          
+             
+        
+        }
+             System.out.println("Your grade is "+grade); */
+             
+             //6
+             int  numbers[5] ={10,20,30,40,50};
+             
+             for(int x:numbers){
+                 System.out.print(x);
+                 System.out.print(",");
+             }
+             System.out.print("\n");
+             
+             String names[]={"James","Larry","Tom","Lacy"};
+             
+             for(String name:names){
+                 System.out.print(name);
+                 System.out.print(",");
+             }
+            }
+            
+         
+  
+}
