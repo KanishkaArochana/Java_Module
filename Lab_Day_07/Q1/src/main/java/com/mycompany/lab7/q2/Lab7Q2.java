0c9b86bd55151e512eaa3c14a1c6c310e525d7e8
KanishkaArochana <135253502+KanishkaArochana@users.noreply.github.com>
2023-07-29 23:45:16 +0530

    Add files via upload

---
+++ Lab_Day_07/Q1/src/main/java/com/mycompany/lab7/q2/Lab7Q2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab7.q2;
+
+/**
+ *
+ * @author kanis
+ */
+public class Lab7Q2 {
+
+    public static void main(String[] args) {
+        Polition s1 = new Polition();
+        s1.speak();
+        
+        Priest p1 = new Priest();
+        p1.speak();
+        
+        Lecturer l1 = new Lecturer();
+        l1.speak();
+    }
+}
---
+++ Lab_Day_07/Q1/src/main/java/com/mycompany/lab7/q2/Priest.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab7.q2;
+
+/**
+ *
+ * @author kanis
+ */
+public class Priest implements Speaker {
+    
+    @Override
+    public void speak(){
+        System.out.println("I am Priest");
+    }
+    
+}
