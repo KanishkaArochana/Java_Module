1ef30a4076f33cc7755291de0e95c76950353ed9
KanishkaArochana <135253502+KanishkaArochana@users.noreply.github.com>
2023-07-31 20:32:05 +0530

    Add files via upload

---
+++ Lab_Day_11_GUI/Q1/src/main/java/com/mycompany/lab_day_11_gui/Lab_Day_11_GUI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_day_11_gui;
+
+/**
+ *
+ * @author kanis
+ */
+public class Lab_Day_11_GUI {
+
+    public static void main(String[] args) {
+        ColorChangeApplication c1 = new ColorChangeApplication();
+        c1.show();
+    }
+}
